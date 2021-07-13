72ed21c4da7dd91df648944059470a48a1ba58ed
Juan Andrade <jpas.juan23@gmail.com>
2021-07-13 17:40:58 -0500

    Insert2 fuction working in ordering.py.

--- ordering.py
+++ ordering.py
@@ -1,7 +1,7 @@
 class Order:
     def __init__(self, ls):
         self._list = ls
-    
+
     def bubble(self):
         for i in range(len(self._list)):
             for j in range(i+1, len(self._list)):
@@ -9,7 +9,7 @@ class Order:
                     aux = self._list[i]
                     self._list[i] = self._list[j]
                     self._list[j] = aux
-    
+
     def insert(self, value):
         self.bubble()
         aux_list = []
@@ -23,7 +23,28 @@ class Order:
         else: self._list.append(value)
         return self._list
 
+    def insert2(self, value):
+        self.bubble()
+        aux_list = []
+        enc = False
+        for pos, ele in enumerate(self._list):
+            if ele > value:
+                enc = True
+                break
+        if enc:
+            for i in range(pos):
+                aux_list.append(self._list[i])
+            aux_list.append(value)
+            for j in range(pos, len(self._list)):
+                aux_list.append(self._list[j])
+            self._list = aux_list
+            self._list = self._list[0:pos] + aux_list + self._list[pos:]
+        else:
+            self._list.append(value)
+        return self._list
+
+
 ord1 = Order([10, 15, 20, 70, 80])
-ord1.insert(50)
+ord1.insert(5)
 ord1.bubble()
 print(ord1._list)
